6968979e6d0bb2f1ece8022a26f20b8d74bc39ad
id44 <ozzss@pm.me>
2023-10-07 05:09:09 +0500

    Initial commit #2

---
+++ src/main.js
@@ -0,0 +1,104 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+
+import HyperAPIDriver            from '@hyperapi/core/driver';
+import HyperAPIRequest           from '@hyperapi/core/request';
+import HyperAPIResponse          from '@hyperapi/core/response';
+import { HyperAPIInternalError } from '@hyperapi/core/api-errors';
+import { createServer }          from 'node:http';
+import IP                        from '@kirick/ip';
+
+import {
+	parseAcceptHeader,
+	parseArguments,
+	parseResponseTo }            from './parse.js';
+
+export default class HyperAPIHttpDriver extends HyperAPIDriver {
+	#httpserver;
+
+	constructor({
+		path = '/api/',
+		port,
+	}) {
+		if (typeof path !== 'string') {
+			throw new TypeError('Property "path" must be a string.');
+		}
+		if (typeof port !== 'number') {
+			throw new TypeError('Property "port" must be a number.');
+		}
+
+		super();
+		this.#httpserver = createServer(async (request, response) => {
+			const url = new URL(request.url, 'http://localhost');
+
+			if (url.pathname.startsWith(path) !== true) {
+				response.writeHead(404);
+				response.end();
+				return;
+			}
+
+			const method = url.pathname.slice(path.length);
+			const args = parseArguments(request, url);
+			const preffered_format = parseAcceptHeader(
+				request.headers['content-type'],
+			);
+
+			if (preffered_format === false) {
+				response.writeHead(415);
+				response.end();
+				return;
+			}
+
+			const hyperApiRequest = new HyperAPIRequest(method, args);
+			hyperApiRequest.set('request', request);
+			hyperApiRequest.set('url', url);
+			hyperApiRequest.set('ip', new IP(request.connection.remoteAddress));
+
+			const hyperAPIResponse = await this.onRequest(hyperApiRequest);
+
+			if (
+				hyperAPIResponse.is_success === false
+				&& hyperAPIResponse.error.httpStatus === undefined
+			) {
+				const internal_error = new HyperAPIResponse(
+					new HyperAPIInternalError(),
+				);
+
+				response.writeHead(
+					500,
+					{
+						'Content-Type': 'application/' + preffered_format,
+					},
+				);
+				response.end(
+					parseResponseTo(
+						preffered_format,
+						internal_error.getResponse(),
+					),
+				);
+				return;
+			}
+
+			response.writeHead(
+				hyperAPIResponse.error?.httpStatus ?? 200,
+				{
+					'Content-Type': 'application/' + preffered_format,
+				},
+			);
+			response.end(
+				parseResponseTo(
+					preffered_format,
+					hyperAPIResponse.getResponse(),
+				),
+			);
+		});
+
+		this.#httpserver.listen(port, () => {
+			console.log(`HyperAPI server running at http://localhost:${port}/`);
+		});
+	}
+
+	destroy() {
+		this.#httpserver.close();
+	}
+}
---
+++ src/parse.js
@@ -0,0 +1,58 @@
+
+import { decode, encode } from 'cbor-x';
+
+export async function parseArguments(request, url) {
+	let args = {};
+
+	if (request.method === 'GET' || request.method === 'HEAD') {
+		for (const [ key, value ] of url.searchParams.entries()) {
+			args[key] = value;
+		}
+	}
+	else if (request.body) {
+		// replace by regexp is faster than split.
+		const type_header = request.headers.get('Content-Type').replace(/;.+/, '');
+		switch (type_header) {
+			case 'application/json':
+				args = await request.json();
+				break;
+			case 'application/x-www-form-urlencoded':
+				args = Object.fromEntries(
+					new URLSearchParams(await request.text()),
+				);
+				break;
+			case 'application/cbor':
+				args = decode(await request.arrayBuffer());
+				break;
+			default:
+				return new Response(
+					'',
+					{
+						status: 404,
+					},
+				);
+		}
+	}
+	return args;
+}
+
+export function parseAcceptHeader(header) {
+	const types = header.split(/\s*(;|,)\s*/);
+	if (types.some((type) => /.+\/(\*|json)/.test(type))) {
+		return 'json';
+	}
+	if (types.some((type) => /.+\/(\*|cbor)/.test(type))) {
+		return 'cbor';
+	}
+	return false;
+}
+
+export function parseResponseTo(format, body) {
+	if (format === 'json') {
+		return JSON.stringify(body);
+	}
+	if (format === 'cbor') {
+		return encode(body);
+	}
+}
+
