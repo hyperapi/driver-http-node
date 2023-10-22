660c1ed83d93b548b0dcd65831b8c5e5ea9b6911
id44 <ozzss@pm.me>
2023-10-22 18:14:36 +0500

    add simple test method

---
+++ src/api/echo.js
@@ -0,0 +1,15 @@
+
+import { OhMyPropsObjectValidator } from 'oh-my-props';
+
+export default function (request) {
+	return {
+		message: `Hello, ${request.args.name}!`,
+	};
+}
+
+export const args = new OhMyPropsObjectValidator({
+	name: {
+		type: String,
+		validator: (value) => value.length > 0 && value.length < 10,
+	},
+});
